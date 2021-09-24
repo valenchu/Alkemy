fa949dc8d036e3c0a37cb0717e6a83ee1fc2cfcb
valenchu <valecassino1@gmail.com>
2021-09-24 08:52:56 -0300

    add archivos

---
+++ src/test/java/com/icons/geographic/start/AlkemyIconsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icons.geographic.start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlkemyIconsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
