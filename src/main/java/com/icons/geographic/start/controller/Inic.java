dad32d4b9ba2efc7c74c594c216528b3a33b3bce
valenchu <valecassino1@gmail.com>
2021-09-27 15:37:48 -0300

    add

---
+++ src/main/java/com/icons/geographic/start/controller/Inic.java
@@ -0,0 +1,17 @@
+package com.icons.geographic.start.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/")
+@RestController
+@CrossOrigin("*")
+public class Inic {
+	
+	@GetMapping
+	public String rest() {
+		return "Todo OK";
+	}
+}
