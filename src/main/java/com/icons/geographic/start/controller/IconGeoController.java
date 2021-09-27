67163bfe7ba123c67b5eca544640df70e0f2d2d1
valenchu <valecassino1@gmail.com>
2021-09-27 13:04:27 -0300

    a

--- src/main/java/com/icons/geographic/start/controller/IconGeoController.java
+++ src/main/java/com/icons/geographic/start/controller/IconGeoController.java
@@ -1,13 +1,11 @@
 package com.icons.geographic.start.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.icons.geographic.start.dto.Icon;
 import com.icons.geographic.start.service.IconGeoService;
 
--- src/main/java/com/icons/geographic/start/service/IconGeoService.java
+++ src/main/java/com/icons/geographic/start/service/IconGeoService.java
@@ -4,7 +4,6 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
-import org.hibernate.mapping.*;
 import com.icons.geographic.start.dto.CiudadPais;
 import com.icons.geographic.start.dto.Icon;
 
@@ -13,7 +12,7 @@ public class IconGeoService {
 
 	public List<Icon> getResp() {
 		return List.of(new Icon(1L, "https://cdn-icons-png.flaticon.com/512/1206/1206798.png", "Marcador",
-				LocalDate.now(), 129F, "A icon Wonderful", new CiudadPais(1L,
+				LocalDate.of(1992, 8, 29), 129F, "A icon Wonderful", new CiudadPais(1L,
 						"https://cdn-icons-png.flaticon.com/512/1717/1717547.png", "Francia", 67000000L, 1265468F)));
 	}
 
